ec721f6a21437b4f221bd59fe09831bb2c972ddc
savioazavedo <savio.azavedo@gmail.com>
2015-04-14 17:29:17 +1200

    Feedr

--- Misc/Feedr/Feedr/obj/Debug/android/src/feedr/FeedActivity.java
+++ Misc/Feedr/Feedr/obj/Debug/android/src/feedr/FeedActivity.java
@@ -10,6 +10,8 @@ public class FeedActivity
 	static {
 		__md_methods = 
 			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onCreateOptionsMenu:(Landroid/view/Menu;)Z:GetOnCreateOptionsMenu_Landroid_view_Menu_Handler\n" +
+			"n_onOptionsItemSelected:(Landroid/view/MenuItem;)Z:GetOnOptionsItemSelected_Landroid_view_MenuItem_Handler\n" +
 			"";
 		mono.android.Runtime.register ("Feedr.FeedActivity, Feedr, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", FeedActivity.class, __md_methods);
 	}
@@ -29,6 +31,22 @@ public class FeedActivity
 	}
 
 	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public boolean onCreateOptionsMenu (android.view.Menu p0)
+	{
+		return n_onCreateOptionsMenu (p0);
+	}
+
+	private native boolean n_onCreateOptionsMenu (android.view.Menu p0);
+
+
+	public boolean onOptionsItemSelected (android.view.MenuItem p0)
+	{
+		return n_onOptionsItemSelected (p0);
+	}
+
+	private native boolean n_onOptionsItemSelected (android.view.MenuItem p0);
 
 	java.util.ArrayList refList;
 	public void monodroidAddReference (java.lang.Object obj)
--- Misc/Feedr/Feedr/obj/Debug/android/src/feedr/feedr/R.java
+++ Misc/Feedr/Feedr/obj/Debug/android/src/feedr/feedr/R.java
@@ -18,7 +18,9 @@ public final class R {
         public static final int upload2=0x7f020004;
     }
     public static final class id {
-        public static final int BtnUploadPostImage=0x7f050005;
+        public static final int BtnUploadPostImage=0x7f050006;
+        public static final int Description=0x7f05000b;
+        public static final int Feedlist=0x7f050011;
         public static final int PostDate=0x7f050002;
         public static final int PostDescription=0x7f050003;
         public static final int PostProfilePic=0x7f050000;
@@ -26,21 +28,19 @@ public final class R {
         public static final int btnLinkToLoginScreen=0x7f050018;
         public static final int btnLinkToRegister=0x7f050010;
         public static final int btnLogin=0x7f05000f;
-        public static final int btnPost=0x7f050006;
+        public static final int btnPost=0x7f050007;
         public static final int btnProfilePic=0x7f050016;
         public static final int btnRegister=0x7f050017;
         public static final int feedImage1=0x7f05000c;
-        public static final int linearLayout1=0x7f050004;
-        public static final int list=0x7f050011;
-        public static final int name=0x7f050008;
+        public static final int imgPostImage=0x7f050004;
+        public static final int linearLayout1=0x7f050005;
+        public static final int name=0x7f050009;
         public static final int password=0x7f05000e;
-        public static final int profilePic=0x7f050007;
+        public static final int profilePic=0x7f050008;
         public static final int textView1=0x7f050015;
-        public static final int timestamp=0x7f050009;
+        public static final int timestamp=0x7f05000a;
         public static final int txtEmail=0x7f050013;
         public static final int txtPassword=0x7f050014;
-        public static final int txtStatusMsg=0x7f05000a;
-        public static final int txtUrl=0x7f05000b;
         public static final int txtUserName=0x7f050012;
         public static final int username=0x7f05000d;
     }
