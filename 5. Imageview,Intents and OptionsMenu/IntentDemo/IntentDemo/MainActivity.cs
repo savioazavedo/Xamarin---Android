94af586dc6acd1721837b7ed119292c03daef176
Savio Azavedo <savio.azavedo@gmail.com>
2016-07-29 13:09:33 +1200

    further changes

--- 5. Imageview,Intents and OptionsMenu/IntentDemo/IntentDemo/MainActivity.cs
+++ 5. Imageview,Intents and OptionsMenu/IntentDemo/IntentDemo/MainActivity.cs
@@ -12,7 +12,18 @@ namespace IntentDemo
 	[Activity (Label = "IntentDemo", MainLauncher = true, Icon = "@drawable/icon")]
 	public class MainActivity : Activity
 	{
-		int count = 1;
+
+        Button btnMap;
+        Button btnCall;
+        Button btnEmail;
+
+        EditText txtPhone;
+        EditText txtLat;
+        EditText txtLong;
+        EditText txtEmailTo;
+        EditText txtEmailSubject;
+        EditText txtEmailMessage;
+
 
 		protected override void OnCreate (Bundle bundle)
 		{
@@ -21,18 +32,57 @@ namespace IntentDemo
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
-			
-			button.Click += delegate {
+            // Get our button from the layout resource,
+            // and attach an event to it
+            btnMap = FindViewById<Button> (Resource.Id.btnMap);
+            btnCall = FindViewById<Button>(Resource.Id.btnCall);
+            btnEmail = FindViewById<Button>(Resource.Id.btnMail);
 
-				var uri = Android.Net.Uri.Parse ("http://www.xamarin.com");
-				var intent = new Intent (Intent.ActionView, uri); 
-				StartActivity (intent);  
-			};
+            txtPhone = FindViewById<EditText>(Resource.Id.txtPhone);
+            txtLat = FindViewById<EditText>(Resource.Id.txtLat);
+            txtLong = FindViewById<EditText>(Resource.Id.txtLong);
+            txtEmailTo = FindViewById<EditText>(Resource.Id.txtEmailTo);
+            txtEmailSubject = FindViewById<EditText>(Resource.Id.txtSubject);
+            txtEmailMessage = FindViewById<EditText>(Resource.Id.txtMessage);
+
+            btnCall.Click += BtnCall_Click;
+            btnMap.Click += BtnMap_Click;
+            btnEmail.Click += BtnEmail_Click;
+
 		}
-	}
+
+        private void BtnEmail_Click(object sender, EventArgs e)
+        {
+            var email = new Intent(Android.Content.Intent.ActionSend);
+
+            email.PutExtra(Android.Content.Intent.ExtraEmail, new string[] { txtEmailTo.Text });
+
+            //You can add a CC as well
+            //email.PutExtra(Android.Content.Intent.ExtraCc,txtEmailTo.Text);
+
+            email.PutExtra(Android.Content.Intent.ExtraSubject, txtEmailSubject.Text);
+
+            email.PutExtra(Android.Content.Intent.ExtraText,txtEmailMessage.Text);
+
+            email.SetType("message/rfc822");
+
+            StartActivity(email);
+        }
+
+        private void BtnMap_Click(object sender, EventArgs e)
+        {
+            var geoUri = Android.Net.Uri.Parse("geo:"+ txtLat.Text + "," + txtLong.Text);
+            var mapIntent = new Intent(Intent.ActionView, geoUri);
+            StartActivity(mapIntent);
+        }
+
+        private void BtnCall_Click(object sender, EventArgs e)
+        {
+            var uri = Android.Net.Uri.Parse("tel:" + txtPhone.Text);
+            var intent = new Intent(Intent.ActionDial, uri);
+            StartActivity(intent);
+        }
+    }
 }
 
 
--- 5. Imageview,Intents and OptionsMenu/IntentDemo/IntentDemo/Resources/Resource.designer.cs
+++ 5. Imageview,Intents and OptionsMenu/IntentDemo/IntentDemo/Resources/Resource.designer.cs
@@ -60,8 +60,32 @@ namespace IntentDemo
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050001
+			public const int btnCall = 2131034113;
+			
+			// aapt resource value: 0x7f050008
+			public const int btnMail = 2131034120;
+			
+			// aapt resource value: 0x7f050004
+			public const int btnMap = 2131034116;
+			
+			// aapt resource value: 0x7f050005
+			public const int txtEmailTo = 2131034117;
+			
+			// aapt resource value: 0x7f050002
+			public const int txtLat = 2131034114;
+			
+			// aapt resource value: 0x7f050003
+			public const int txtLong = 2131034115;
+			
+			// aapt resource value: 0x7f050007
+			public const int txtMessage = 2131034119;
+			
 			// aapt resource value: 0x7f050000
-			public const int myButton = 2131034112;
+			public const int txtPhone = 2131034112;
+			
+			// aapt resource value: 0x7f050006
+			public const int txtSubject = 2131034118;
 			
 			static Id()
 			{
